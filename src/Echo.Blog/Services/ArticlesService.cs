39276317a8682550974c5b22a1a8af35e8152488
rwecho <rwecho@live.com>
2021-03-05 08:27:03 +0800

    add test articles.

--- src/Echo.Blog/Services/ArticlesService.cs
+++ src/Echo.Blog/Services/ArticlesService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Bogus;
 using Microsoft.Extensions.Logging;
@@ -21,27 +22,29 @@ namespace Echo.Blog.Services
 
         public async Task<IReadOnlyList<Article>> GetArticles()
         {
-           var articles = new Faker<Article>()
-                .RuleFor(o=>o.Id, f=> Guid.NewGuid())
-                .RuleFor(o=>o.Title, f=> f.Lorem.Sentence())
-                .RuleFor(o=>o.Content, f=> f.Lorem.Paragraphs(10))
-                .RuleFor(o=>o.CreationTime, f=> f.Date.Past())
-                .Generate( 50);
-           return articles.ToArray();
+            var tags = this.Faker.Random.Bytes(50)
+                .Select(o => this.Faker.Commerce.Color())
+                .ToArray();
+            var articles = new Faker<Article>()
+                .RuleFor(o => o.Id, f => Guid.NewGuid())
+                .RuleFor(o => o.Title, f => f.Lorem.Sentence())
+                .RuleFor(o => o.Content, f => f.Lorem.Paragraphs(10))
+                .RuleFor(o => o.CreationTime, f => f.Date.Past())
+                .RuleFor(o => o.Tags, f => f.PickRandom(tags,5).ToArray())
+                .Generate(50);
+            return articles.ToArray();
         }
-     }
+    }
 
     public class Article
     {
         public Guid Id { get; set; }
 
-        public string Title { get; set; }
-        public string Content { get; set; }
+        public string Title { get; set; } = null!;
+        public string Content { get; set; } = null!;
 
         public DateTime CreationTime { get; set; }
 
-        public string[] Tags { get; set; }
-
-
+        public string[]? Tags { get; set; }
     }
 }
